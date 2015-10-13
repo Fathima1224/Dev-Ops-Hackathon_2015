a8415ac4cd4ae12b8a85039ac934cabf1d4a6595
fathima <fathima.sams@gmail.com>
2015-10-13 18:09:18 +0530

    login issue fixed

--- app/src/main/java/com/trimble/fithaya/devops_dashboard/LoginActivity.java
+++ app/src/main/java/com/trimble/fithaya/devops_dashboard/LoginActivity.java
@@ -1,46 +1,52 @@
 package com.trimble.fithaya.devops_dashboard;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
 ////added comment
-public class LoginActivity extends AppCompatActivity {
-    private EditText txtUserName = (EditText) findViewById(R.id.username);
-    private EditText txtPassword = (EditText) findViewById(R.id.password);
-    private Button btnSignin = (Button) findViewById(R.id.sign_in_button);
+public class LoginActivity extends Activity {
+    private EditText txtUserName;
+    private EditText txtPassword;
+    private Button btnSignin;
+    private TextView lblStatus;
 
-   btnSignin.setOnClickListener (new OnClickListener() {
 
-
-     //   btnSignin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        txtUserName = (EditText) findViewById(R.id.username);
+        txtPassword = (EditText) findViewById(R.id.password);
+        btnSignin = (Button) findViewById(R.id.sign_in_button);
+        //lblStatus = (TextView) findViewById(R.id.login_status);
+        btnSignin.setOnClickListener(new OnClickListener() {
 
             @Override
-        public void onClick (View v){
-            String username = txtUserName.getText().toString();
-            String password = txtPassword.getText().toString();
-            try {
-                if (username.length() > 0 && password.length() > 0) {
-                    if (username.equals("admin") && password.equals("admin123")) {
+            public void onClick(View v) {
+                String username = txtUserName.getText().toString();
+                String password = txtPassword.getText().toString();
+                try {
+                    if (username.equalsIgnoreCase("USER") && password.equals("12345")) {
+                        //lblResult.setText("Login successful");
+                        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(loginIntent);
+                    } else {
 
-
+                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
                     }
+                } catch (Exception e) {
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
-            } catch (Exception e) {
-                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
-        }
+        });
+    }
 
-
-    });
 
 }
